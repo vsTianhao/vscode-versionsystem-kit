4dd5b055bce3a48297c4128046e83ae54ba181da
vsTianhao <futianhao@versionsystem.com>
2022-01-05 10:29:04 +0800

    :sparkles: add show log panel task
    Signed-off-by: vsTianhao <futianhao@versionsystem.com>

--- src/cmds/OtherCommands.ts
+++ src/cmds/OtherCommands.ts
@@ -9,6 +9,9 @@ export default async function (type = "config"): Promise<void> {
             content: JSON.stringify(vscode.workspace.getConfiguration("vskit"), null, "\t")
         })
         vscode.window.showTextDocument(doc, { preview: true })
+    } else if (type === "log") {
+        logger.info("显示Output面板")
+        logger.info("完毕")
     } else if (type === "close") {
         logger.log("尝试process.exit, 当然这会引起vscode报错")
         process.exit(0)
--- src/tasks/BasicTask.ts
+++ src/tasks/BasicTask.ts
@@ -14,13 +14,13 @@ export default class BasicTask {
 
     list(task: string): TaskItemBtn[] {
         if (task === "build") {
-            return [new TaskItemBtn("build-all", "全部编译"), new TaskItemBtn("build-html", "单独编译index.html")]
+            return [new TaskItemBtn("build-all", "编译"), new TaskItemBtn("build-html", "单独编译index.html")]
         } else if (task === "run") {
             return [new TaskItemBtn("run-dev", "运行开发服务器"), new TaskItemBtn("run-shutdown", "关闭开发服务器")]
         } else if (task === "upload") {
             return [new TaskItemBtn("upload-dist", "上传./dist到服务器"), new TaskItemBtn("upload-assert", "上传./assert到服务器")]
         } else if (task === "others") {
-            return [new TaskItemBtn("others-config", "查看当前环境的配置"), new TaskItemBtn("others-close", "尝试process.exit")]
+            return [new TaskItemBtn("others-config", "查看当前环境的配置"), new TaskItemBtn("others-log", "查看Output"), new TaskItemBtn("others-close", "尝试process.exit")]
         }
         return []
     }
